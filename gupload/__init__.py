3119c601f73625c35c57bebf946bf263a2ec99b5
Pavel Hübner <pavel.hubner@bigclown.com>
2019-07-30 22:40:16 +0200

    Fix code style

--- gupload/__init__.py
+++ gupload/__init__.py
@@ -4,9 +4,13 @@ import googleapiclient.discovery
 
 __version__ = 'v1.0.0'
 
+
 @click.command()
 @click.option('-d', '--folder', help='Target folder identifier.', metavar='ID')
-@click.option('-f', '--file', 'files', nargs=2, type=click.Tuple([str, click.File('rb')]), multiple=True, help='Input file(s) to be uploaded.', metavar='<NAME PATH>...')
+@click.option('-f', '--file', 'files', nargs=2,
+              type=click.Tuple([str, click.File('rb')]),
+              multiple=True, help='Input file(s) to be uploaded.',
+              metavar='<NAME PATH>...')
 @click.version_option(version=__version__)
 def main(folder, files):
     try:
@@ -16,7 +20,9 @@ def main(folder, files):
             body = {'name': name}
             if folder is not None:
                 body['parents'] = [folder]
-            media_body = apiclient.http.MediaIoBaseUpload(file, mimetype='application/octet-stream')
-            service.files().create(body=body, media_body=media_body, fields='id').execute()
+            media_body = apiclient.http.MediaIoBaseUpload(
+                file, mimetype='application/octet-stream')
+            service.files().create(
+                body=body, media_body=media_body, fields='id').execute()
     except KeyboardInterrupt:
         pass
